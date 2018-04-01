10bef1e0960eeb1bf5592bd58cfc8c389418f4ee
Attila Racz <racz.attila89@gmail.com>
2018-04-01 21:41:08 +0200

    answers appear by ajax

--- src/main/resources/public/js/dom.js
+++ src/main/resources/public/js/dom.js
@@ -4,17 +4,24 @@ window.onload = function(){
         type: 'GET',
         dataType: 'json',
         success: function(poll){
-            $("#poll").append(`<h2>${poll.question}</h2>`
+            $("#poll").append(`${poll.question}`
             )
-        }});
+        }
+    });
 
     $.ajax({
         url: '/getanswers',
         type: 'GET',
         dataType: 'json',
         success: function(answers){
-            $("#poll").append(`<h2>${answers[0].answer}</h2>`
-            )
-        }});
-
+            var index = 1;
+            $.each(answers, function(i, oneAnswer){
+                $("#answer").append(`
+                    <label>${answers[i].answer}</label>
+                    <input type="radio" name="answer" class="answer" id="${index}" />
+                    <br>`)
+                    index++;
+            })
+        }
+    });
 };
