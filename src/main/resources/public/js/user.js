c743f9f1d5bccbb9e43cddf890d83d0d46780680
Attila Racz <racz.attila89@gmail.com>
2018-05-10 10:38:13 +0200

    ajax ready to send registration data to the server

--- src/main/resources/public/js/user.js
+++ src/main/resources/public/js/user.js
@@ -1,6 +1,18 @@
 window.onload = function(){
-    $('.login_error').hide();
+    fireLoginButton();
+    fireRegisterButton();
+};
 
+const errorlist = {
+    "allFieldsRequired": "All fields are required!",
+    "passwordMismatch": "Passwords do not match!",
+    "invalidName": "Your name contains invalid characters!",
+    "emailExists": "Your email already exists!",
+    "emailInvalid": "That does not look like a valid email!"
+};
+
+function fireLoginButton() {
+    $('.login_error').hide();
     $('.login-button').click(function(event){
         event.preventDefault();
         let data = {
@@ -28,4 +40,41 @@ window.onload = function(){
             }
         });
     });
-};
+}
+
+function fireRegisterButton() {
+    $('.register_errors').hide();
+    $('.register-button').click(function(event){
+        event.preventDefault();
+        let data = {
+            'email': $('#register_email').val(),
+            'username': $('#register_username').val(),
+            'password': $('#register_password').val(),
+            'passwordcheck': $('#register_password_check').val()
+        };
+        $.ajax({
+            type: 'POST',
+            contentType: 'application/JSON',
+            url: '/api/register',
+            data: JSON.stringify(data),
+            success: function (response) {
+                console.log(response);
+                $('.errors').empty();
+                $('#statusMessages').empty();
+                if(JSON.parse(response)["valid"] === true){
+                    $(location).attr('href', window.location.href + "/dashboard");
+                } else {
+                    $.each(JSON.parse(response), function(key, value) {
+                        if(value === true) {
+                            $('.errors').append("<li>" + errorlist[key] + "</li>");
+                        }
+                        $('.register_errors').show();
+                    });
+                }
+            },
+            error: function(response) {
+                console.log(response);
+            }
+        });
+    });
+}
